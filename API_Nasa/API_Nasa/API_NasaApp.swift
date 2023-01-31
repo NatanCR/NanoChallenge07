bd0dc9449c6afd15ad6026d68cc2cc14b2d41917
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-01-31 15:51:49 -0300

    initial commit

---
+++ API_Nasa/API_Nasa/API_NasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_NasaApp.swift
+//  API_Nasa
+//
+//  Created by Natan de Camargo Rodrigues on 31/01/23.
+//
+
+import SwiftUI
+
+@main
+struct API_NasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
