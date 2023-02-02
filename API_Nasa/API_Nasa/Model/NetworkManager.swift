bed22a16cda17306ee9b856e855b64535d71060e
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-02-02 15:38:24 -0300

    api

--- API_Nasa/API_Nasa/Model/NetworkManager.swift
+++ API_Nasa/API_Nasa/Model/NetworkManager.swift
@@ -10,20 +10,55 @@ import SwiftUI
 
 class NetworkManager: ObservableObject {
     
-    @Published var planets = [PlanetInfos]()
     
-    func fetchData(planetName: String) {
-        let name = "\(planetName)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
-        let url = URL(string: "https://api.api-ninjas.com/v1/planets?name="+name!)!
-        var request = URLRequest(url: url)
-        request.setValue("YOUR_API_KEY", forHTTPHeaderField: "X-Api-Key")
-        let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
-            guard let data = data else { return }
-            print(String(data: data, encoding: .utf8)!)
+    // configura o cabeçalho com a key
+    let headers = [
+        "X-RapidAPI-Key": "2e68fe9a08msha4b151812270feep1fae83jsn67e1e76b4267",
+        "X-RapidAPI-Host": "planets-info-by-newbapi.p.rapidapi.com"
+    ]
+    
+    // Solicita o requisição
+    let request = NSMutableURLRequest(url: NSURL(string: "https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planet/list")! as URL,
+                                            cachePolicy: .useProtocolCachePolicy,
+                                        timeoutInterval: 10.0)
+
+    // Declara o tipo de requisição
+    request.httpMethod = "GET"
+    request.allHTTPHeaderFields = headers
+
+
+    // Faz a ligação com a API
+    let session = URLSession.shared
+    let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+        guard let responseData = data else { return }
+        
+        do {
+            if let responseString = String(data: responseData, encoding: .utf8) {
+                print(responseString)
+            }
         }
-        task.resume()
-    }
-  
+    })
+
+    //aguarda o dado ser retornado
+    dataTask.resume()
+    
+    
+    
+//
+//    @Published var planets = [PlanetInfos]()
+//
+//    func fetchData(planetName: String) {
+//        let name = "\(planetName)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+//        let url = URL(string: "https://api.api-ninjas.com/v1/planets?name="+name!)!
+//        var request = URLRequest(url: url)
+//        request.setValue("YOUR_API_KEY", forHTTPHeaderField: "X-Api-Key")
+//        let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
+//            guard let data = data else { return }
+//            print(String(data: data, encoding: .utf8)!)
+//        }
+//        task.resume()
+//    }
+//
     
     
     //    func fetchData(planetName: String) {
