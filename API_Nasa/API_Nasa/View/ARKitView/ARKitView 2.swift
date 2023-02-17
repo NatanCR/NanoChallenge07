9e283aa2c1faa73c71ad08e23fd3cd2686bab67c
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-02-16 21:40:30 -0300

    .

---
+++ API_Nasa/API_Nasa/View/ARKitView/ARKitView 2.swift
@@ -0,0 +1,94 @@
+//
+//  ARKitView.swift
+//  API_Nasa
+//
+//  Created by Natan de Camargo Rodrigues on 14/02/23.
+//
+
+import Foundation
+import ARKit
+import SwiftUI
+
+// MARK: - ARViewIndicator
+struct ARViewIndicator: UIViewControllerRepresentable {
+   typealias UIViewControllerType = ARKitView
+    @State var planetDetails: PlanetInfos
+   
+   func makeUIViewController(context: Context) -> ARKitView {
+       return ARKitView(planetInfos: planetDetails)
+   }
+   func updateUIViewController(_ uiViewController:
+   ARViewIndicator.UIViewControllerType, context:
+   UIViewControllerRepresentableContext<ARViewIndicator>) { }
+}
+
+class ARKitView: UIViewController, ARSCNViewDelegate {
+    var planetInfos: PlanetInfos
+    
+    init(planetInfos: PlanetInfos) {
+        self.planetInfos = planetInfos
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    var arView: ARSCNView {
+        return self.view as! ARSCNView
+    }
+    override func loadView() {
+        self.view = ARSCNView(frame: .zero)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        arView.delegate = self
+        arView.scene = SCNScene()
+        createPlanetSphere()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+          super.viewDidAppear(animated)
+       }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+
+        // Run the view's session
+        arView.session.run(configuration)
+        arView.delegate = self
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // Pause the view's session
+        arView.session.pause()
+    }
+    
+    func createPlanetSphere() {
+        let sphere = SCNSphere(radius: 0.2)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "\(planetInfos.name).jpeg")
+        sphere.materials = [material]
+        
+        let node = SCNNode()
+        node.position = SCNVector3(0, 0.1, -0.5)
+        node.geometry = sphere
+        arView.scene.rootNode.addChildNode(node)
+        addAnimation(node: node)
+        arView.automaticallyUpdatesLighting = true
+        
+    }
+    
+    func addAnimation(node: SCNNode) {
+        let rotateOne = SCNAction.rotateBy(x: 0, y: CGFloat(Float.pi), z: 0, duration: 5.0)
+        let repeatForever = SCNAction.repeatForever(rotateOne)
+        node.runAction(repeatForever)
+    }
+    
+}
